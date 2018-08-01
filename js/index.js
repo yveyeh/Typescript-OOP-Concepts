8f2f3fc3bd5879b2a476577db36842e7bde343be
Didier yufenyuy <didier.yufenyuy@synepsys.com>
2018-08-01 02:40:48 +0100

    package.json and index.js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var classes_1 = require("./classes");
+var a_person = new classes_1.Person("Didi", "Yufen", 25);
+console.log('First Name: ' + a_person.get_first_name());
---
+++ ts/index.ts
@@ -0,0 +1,6 @@
+import {Person} from './classes';
+
+let a_person = new Person("Didi", "Yufen", 25);
+console.log('First Name: ' + a_person.get_first_name());
+
+
