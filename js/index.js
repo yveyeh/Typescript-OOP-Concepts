c9d291bfb0b63fb1b12f88b91845a0b5e520626c
Didier yufenyuy <didier.yufenyuy@synepsys.com>
2018-08-01 03:04:52 +0100

    installed loadash with type definitions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var classes_1 = require("./classes");
+var _ = __importStar(require("lodash"));
 var a_person = new classes_1.Person("Didi", "Yufen", 25);
 console.log('First Name: ' + a_person.get_first_name());
+var array = [1, 2, 3, 4, 5];
+console.log(_.reverse(array));
--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,8 @@
 import {Person} from './classes';
+import * as _ from 'lodash';
 
 let a_person = new Person("Didi", "Yufen", 25);
 console.log('First Name: ' + a_person.get_first_name());
 
-
+var array = [1, 2, 3, 4, 5];
+console.log(_.reverse(array));
